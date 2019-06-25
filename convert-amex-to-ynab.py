d6b3ee3ce3dcd70938e0f51e0d0348237690062f
Douglas Muth <dmuth@dmuth.org>
2019-06-24 22:47:20 -0400

    Fixed dates

--- convert-amex-to-ynab.py
+++ convert-amex-to-ynab.py
@@ -19,12 +19,24 @@ print("Date, Payee, Memo, Amount")
 
 #
 # Loop through rows and print in the format YNAB wants.
-# Flip the sign on the amount because charges are expenses, not income.
 #
 for row in csv:
+	
+	#
+	# The date is in the format "06/01/2019  Sat", and we just
+	# want the first part of that.
+	#
 	date = row[0]
+	fields = date.split(" ")
+	date = fields[0]
+
 	payee = row[2]
+
+	#
+	# Flip the sign on the amount because charges are expenses, not income.
+	#
 	amount = "{:.2f}".format(float(row[7]) * -1)
+
 	print('{},"{}",,{}'.format(date, payee, amount))
 
 f.close()
